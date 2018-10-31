a3aaff8a4f8c12294ceaa150cf871dfba21ab927
Ekam Walia <ekamwalia@gmail.com>
2018-10-31 18:29:58 +0530

    Update hello_hacktober.go

--- hello_hacktober.go
+++ hello_hacktober.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
+		fmt.Fprintf(w, "Hello, Hacktober!!")
 	})
 
 	http.ListenAndServe(":8080", nil)
